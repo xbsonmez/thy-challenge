50b052f55d22bdf5074ce99ec0d6f896d829c203
BURAK SONMEZ <42543196+xbsonmez@users.noreply.github.com>
2023-05-30 11:35:22 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],base: "/thy-challenge",
   resolve: {
     alias: [
       { find: '@', replacement: '/src'},
